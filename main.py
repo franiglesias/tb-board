23856bdb7be4c75bb0cbe8f783728733896a4a51
Fran Iglesias <franiglesias@mac.com>
2021-01-30 17:13:13 +0100

    Introduce basic readme

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from component.analog_input.photoresistor import PhotoResistor
 from component.led.led import Led
 from component.analog_digital_converter import AnalogDigitalConverter
 
-if __name__ == '__main__':
-    print('Program is starting ... ')
 
+def thermometer():
     board = Board()
-
     adc = AnalogDigitalConverter()
-    #
+
     thermistor = PhotoResistor(analog_digital_converter=adc, channel=0, voltage=3.3)
+
     red = Led(Pin(11), 'Red')
     red.connect_input(thermistor)
 
     board.connect(adc)
     board.connect(thermistor)
     board.connect(red)
-
     try:
         board.check()
         board.loop()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         board.destroy()
+
+
+if __name__ == '__main__':
+    thermometer()
